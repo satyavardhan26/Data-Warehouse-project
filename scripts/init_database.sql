7f2516b64be163f45ac1224b025e32d9cb7a0eee
satya vardhan <136916512+satyavardhan26@users.noreply.github.com>
2025-05-23 16:57:48 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,26 @@
+/*
+Creating Database and Schemas
 
+Purpose of the Script 
+This Script creates a new folder named "DataWarehouse" The Scripts has three Schemas 
+Within Database Bronze,Silver,Gold
+
+--Create Database named as DataWarehouse
+CREATE database DataWarehouse;
+
+--Using same databse
+Use DataWarehouse;
+
+--Crating schemas for different layers
+  
+CREATE Schema Bronze;
+Go
+CREATE Schema Silver;
+GO
+CREATE Schema Gold;
+
+
+
+
+
+*/
